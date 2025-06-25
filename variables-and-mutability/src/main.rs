30d86b20500c0cad503ee384fd67de6696269e22
michail.almyros <m.almyros@gmail.com>
2025-06-25 14:28:41 +0100

    - Adding gitignore file
    - Adding first example for creating immutable variables and using string interpolation

---
+++ variables-and-mutability/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Allocating values to immutable variables
+    let apples: i32 = 5;
+    let oranges: i32 = 14 + 6;
+    let fruits = apples + oranges;
+
+    // String interpolation
+    println!("My garden has {fruits} fruits");
+}
