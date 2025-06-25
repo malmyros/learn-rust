4170ab242859a10d716740827b7f9bbca5c6a56e
michail.almyros <m.almyros@gmail.com>
2025-06-25 14:33:31 +0100

    - Example of ignoring compiler warnings for unused variables

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -2,9 +2,13 @@ fn main() {
     // Allocating values to immutable variables
     let apples: i32 = 5;
     let oranges: i32 = 14 + 6;
-    let fruits = apples + oranges;
+    let fruits: i32 = apples + oranges;
 
     // String interpolation
     println!("My garden has {fruits} fruits");
     println!("Apples are {0} and oranges are {1}", apples, oranges);
+    
+    // Example of an unused variable prefixed with _
+    // to ignore compiler warnings (I wouldn't use that)
+    let _test = "Some text to ignore warnings";
 }
