5e5c16d291f40c7f2d6f5330ae64047fcc2fef25
michail.almyros <m.almyros@gmail.com>
2025-06-25 16:57:12 +0100

    - Example of creating signed and unsigned integers

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    /*
+    Rust automatically infers the type, but we can also assign
+    it if we want to be specific
+    */
+
+    // Signed Integers can be negative and positive numbers
+    let eight_bit: i8 = -112;
+    let sixteen_bit: i16 = -32767;
+
+    // Unsigned Integers can be zero or positive numbers
+    let eight_bit_unsinged: u8 = 112;
+    let sixteen_bit_unsinged: u16 = 32768;
+    
+    // Alternative syntax (Not commonly used)
+    let some_variable = 8i16;
 }
