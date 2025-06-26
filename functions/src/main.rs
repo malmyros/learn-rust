294a1a897b0323c1ac6cdd3dbfc5ad43de1c05da
michail.almyros <m.almyros@gmail.com>
2025-06-26 12:53:47 +0100

    - Example of blocks in functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,11 +2,22 @@ fn main() {
     open_store("Los Angeles");
     bake_pizza(32, "anchovies");
     swimming_in_profit();
-    println!("{}", square(2))
+    println!("{}", square(2));
+
+    // Returning a unit
+    let result: () = ();
+    mystery();
+
+    let multiplier: i32 = 3;
+    let calculation: i32 = {
+        let value: i32 = 5 + 4;
+        value * multiplier
+    };
+    println!("Result: {calculation}");
 }
 
 fn open_store(neighborhood: &str) {
-    println!("Opening a store, in {neighborhood}");    
+    println!("Opening a store, in {neighborhood}");
 }
 
 fn bake_pizza(number: i32, toppings: &str) {
@@ -14,9 +25,13 @@ fn bake_pizza(number: i32, toppings: &str) {
 }
 
 fn swimming_in_profit() {
-    println!("So much $$$, swimming in profit")
+    println!("So much $$$, swimming in profit");
 }
 
 fn square(number: i32) -> i32 {
     number * number
+}
+
+fn mystery() -> () {
+    println!("Function that returns a unit");
 }
