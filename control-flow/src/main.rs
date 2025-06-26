74df1489643b09fb18596c8f04b78c50e2ad04fd
michail.almyros <m.almyros@gmail.com>
2025-06-26 17:07:30 +0100

    - Example of replacing if else statements with matching

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,17 +1,21 @@
 fn main() {
-    
     let numbers: [i32; 5] = [1, 2, 3, 4, 5];
     for number in numbers.iter() {
         even_or_odd(*number)
     }
-    
+
+    let condition = 20 > 10;
+    let result = match condition {
+        true => 20,
+        false => 10,
+    };
+    println!("The result is {}", result);
 }
 
-fn even_or_odd(i: i32) -> () {
-    let is_even = i % 2 == 0;
-    if is_even {
-        println!("{} is even", i);
-    } else {
-        println!("{} is odd", i);
+fn even_or_odd(value: i32) -> () {
+    let is_even = value % 2 == 0;
+    match is_even {
+        true => println!("{} is Even!", value),
+        false => println!("{} is Odd!", value),
     }
 }
