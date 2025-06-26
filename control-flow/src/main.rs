9938918dbcc42ea12758fc8b0772ebabfb34d72a
michail.almyros <m.almyros@gmail.com>
2025-06-26 16:56:55 +0100

    - Example of simple if statements and iterating through numbers to print whether they are odd or even

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    for number in numbers.iter() {
+        even_or_odd(*number)
+    }
+    
+}
+
+fn even_or_odd(i: i32) -> () {
+    let is_even = i % 2 == 0;
+    if is_even {
+        println!("{} is even", i);
+    } else {
+        println!("{} is odd", i);
+    }
 }
