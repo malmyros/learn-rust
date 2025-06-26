7210d6c80b750b36ab252f754d627ec578ff216d
michail.almyros <m.almyros@gmail.com>
2025-06-26 12:48:19 +0100

    - Example of defining functions, with parameters and implicit returns

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    open_store("Los Angeles");
+    bake_pizza(32, "anchovies");
+    swimming_in_profit();
+    println!("{}", square(2))
+}
+
+fn open_store(neighborhood: &str) {
+    println!("Opening a store, in {neighborhood}");    
+}
+
+fn bake_pizza(number: i32, toppings: &str) {
+    println!("Baking {number} {toppings} pizza!");
+}
+
+fn swimming_in_profit() {
+    println!("So much $$$, swimming in profit")
+}
+
+fn square(number: i32) -> i32 {
+    number * number
 }
