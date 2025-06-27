33efbcca08fab44d703980941790d59f26f346e7
michail.almyros <m.almyros@gmail.com>
2025-06-27 12:41:15 +0100

    - Example of the copy trait

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,10 @@
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    /*
+    In rust variables implement the copy trait
+    which means in the following example we are
+    going to have the value of 2025 twice in the stack
+    */
+    let time = 2025;
+    let year = time;
 }
