7e4a8d25253d0702f091a1f0e4ac51dcff7219db
michail.almyros <m.almyros@gmail.com>
2025-06-27 12:48:05 +0100

    - Example of how &str and String work in Rust

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,4 +7,21 @@ fn main() {
     */
     let time = 2025;
     let year = time;
+
+    /*
+    The &str is embedded directly to the compiler executable,
+    and it's not stored either in the stack or the heap, this
+    happens because the value is already know at the compile time
+     */
+    let food: &str = "past";
+
+    /*
+    String is a different type in rust which is used
+    to dynamically build strings as the value of the string
+    is not known at the compile time, it acts as String builder
+    this type would be stored in the heap
+     */
+    let text = String::new();
+    let candy = String::from("KitKat");
+    
 }
