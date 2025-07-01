5e16d1a68e33c7712b9e4cc4e43c3f5100b64fc5
michail.almyros <m.almyros@gmail.com>
2025-07-01 13:24:51 +0100

    - Example of Lifetimes and Mutable Reference Restrictions

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-
     /*
     Rust allows unlimited amount of immutable references and borrowers
     as there is no risk in reading the value, when we can't mutate it.
@@ -8,4 +7,18 @@ fn main() {
     let ref1: &String = &car;
     let ref2: &String = &car;
     println!("Ref1: {} and Re2f: {} and Inline Ref: {}", ref1, ref2, &car);
+
+    /*
+    A lifetime is a construct the compiler (or more specifically, its borrow checker) 
+    uses to ensure all borrows are valid. Specifically, a variable's lifetime begins 
+    when it is created and ends when it is destroyed. While lifetimes and scopes are 
+    often referred to together, they are not the same.
+     */
+    let mut plane: String = String::from("Blue");
+    let ref3: &mut String = &mut plane;
+    ref3.push_str(" and Green");
+    let ref4: &String = &ref3;
+    println!("Ref3: {}", ref3);
+    println!("Ref4: {}", ref4);
+    println!("Plane: {}", plane);
 }
