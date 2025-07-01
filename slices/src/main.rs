35436b2d81f9cb08ad1432c5c3198f8a0d92684c
michail.almyros <m.almyros@gmail.com>
2025-07-01 16:18:38 +0100

    - Example of creating a string slice from a String

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+
+    let action_hero: String = String::from("Arnold Schwarzenegger");
+    assert_eq!(action_hero.len(), 21);
+    
+    let first_name: &str = &action_hero[0..6];
+    let last_name: &str = &action_hero[6..21];
+    println!("First Name: {}", first_name);
+    println!("Last Name: {}", last_name);
 }
