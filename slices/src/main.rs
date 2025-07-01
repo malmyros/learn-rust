3aed94542d07919148f396cb58db31904fe44c7f
michail.almyros <m.almyros@gmail.com>
2025-07-01 16:34:23 +0100

    - Example of String Slice Lengths

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -16,11 +16,28 @@ fn main() {
     Because we are using &str the value of action here will be
     embedded in the final executable and therefore when we return
     the slice we won't create a dangling reference as the value
-    will be referenced from the memory 
+    will be referenced from the memory
      */
     let some_part: &str = {
         let action_hero: &str = "Arnold Schwarzenegger";
         &action_hero[0..6]
     };
     println!("Some Part: {}", some_part);
+
+    /*
+    The length of a string slice refers to a count
+    of its bytes, it's characters
+     */
+    let food: &str = "Pizza";
+    println!("Length: {}", food.len());
+
+    let pizza_slice: &str = &food[0..3];
+    println!("Length: {}", pizza_slice.len());
+
+    /*
+    The length here is now 4 as it occupies 4 bytes
+    in memory and not 1 as in length of characters
+    */
+    let crab: &str = "🦀";
+    println!("Length: {}", crab.len());
 }
