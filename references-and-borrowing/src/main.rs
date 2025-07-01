6408ac824d5c9e053f2b65df32cc34cf30e473c3
michail.almyros <m.almyros@gmail.com>
2025-07-01 13:12:28 +0100

    - Example of multiple immutable references using the & borrow operator

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+
+    /*
+    Rust allows unlimited amount of immutable references and borrowers
+    as there is no risk in reading the value, when we can't mutate it.
+     */
+    let car: String = String::from("Red");
+    let ref1: &String = &car;
+    let ref2: &String = &car;
+    println!("Ref1: {} and Re2f: {} and Inline Ref: {}", ref1, ref2, &car);
 }
