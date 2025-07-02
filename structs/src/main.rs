3ae2044ab73091ffba3293fdd325147dd9dbf4b4
michail.almyros <m.almyros@gmail.com>
2025-07-02 17:09:48 +0100

    - Example of overwriting struct fields

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -16,26 +16,46 @@ fn main() {
         price: 5.64,
         is_hot: true,
     };
-    
+
     println!("mocha name: {}", mocha.name);
     println!("mocha price: £{:.2}", mocha.price);
     println!("mocha is_hot: {}", mocha.is_hot);
-    
+
     /*
     Doing this will move ownership of the name field
     from the mocha object to the my_favourite_coffee
-    
+
     let my_favourite_coffee = mocha.name;
-    
+
     Before of this the compiler won't allow us to
     do something like this:
-    
+
     println!("{}", mocha.name);
      */
+
+    let mut beverage = Coffee {
+        name: String::from("Mocha"),
+        price: 5.64,
+        is_hot: false,
+    };
+
+    beverage.name = String::from("Latte Caramel Macchiato");
+    beverage.price = 6.99;
+    beverage.is_hot = false;
+    
+    print_coffee(&mocha);
+    print_coffee(&beverage);
 }
 
 struct Coffee {
     name: String,
     price: f64,
     is_hot: bool,
+}
+
+fn print_coffee(coffee: &Coffee) {
+    println!(
+        "Coffee: {}, costs {}, and {}",
+        coffee.name, coffee.price, if coffee.is_hot {"it's hot"} else {"it's not hot"}
+    );
 }
