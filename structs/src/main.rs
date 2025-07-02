c6f259f0d2a44ab159c8844b6534b93facc858cf
michail.almyros <m.almyros@gmail.com>
2025-07-02 16:56:12 +0100

    - Example of defining a struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,2 +1,19 @@
+/*
+A struct (structure) is a container for
+related pieces of data.
+
+Types of Structs are:
+    - Named Field Structs
+    - Tuple-Like Structs
+    - Unit-Like Structs
+ */
+
 fn main() {
+    
+}
+
+struct Coffee {
+    name: String,
+    price: f64,
+    is_hot: bool,
 }
